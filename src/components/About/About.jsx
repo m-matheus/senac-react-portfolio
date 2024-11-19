d70b8ed13b77ade9adfd98dffbc80790043c8b83
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-11-19 16:12:20 -0300

    criei uma linha

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -18,6 +18,7 @@ export const About = () => {
             <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
             <div className={styles.aboutItemText}>
               <h3>Frontend Developer</h3>
+              
               <p>
                 I'm a frontend developer with experience in building responsive
                 and optimized sites
